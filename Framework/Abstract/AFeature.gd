d7e4e1cd7215778461c560cbe3bcc6aa3d77e25a
turbohermit <daanlast123@gmail.com>
2024-01-07 15:13:58 +0100

    + [documentation] add comments to feature and model resource

--- Framework/Abstract/AFeature.gd
+++ Framework/Abstract/AFeature.gd
@@ -1,3 +1,5 @@
+# AFeature is the base class for Features.
+# Features are Resources that act as a factory for Model, Controller and View initialization.
 class_name AFeature
 extends Resource
 func get_class_name(): return "AFeature"
@@ -5,28 +7,33 @@ func get_class_name(): return "AFeature"
 var m_root: Control
 var m_viewCollection: ViewCollection
 var m_controllers: Array[AController]
-var m_featureInitializer: FeatureInitializer
 
-func initialize(p_root: Control, p_createInstance: bool = true, p_featureInitializer: FeatureInitializer = null) -> AFeature:
-	var feature = self
-	if p_createInstance:
-		feature = self.duplicate()
+# Creates and initializes an instance of this Feature Resource and returns it.
+func initialize(p_root: Control) -> AFeature:
+	var feature = self.duplicate()
 	
 	feature.m_root = p_root
-	feature.m_featureInitializer = p_featureInitializer
 	feature.m_viewCollection = ViewCollection.new()
+	
 	feature.init_views()
 	feature.init_controllers()
 	feature.on_initialized()
+	
 	print(str(feature.get_class_name(), " initialized."))
 	return feature as AFeature
 
+# Creates and tracks Controller instaces from the Feature.
 func kickstart(p_controller: AController):
 	p_controller.m_root = m_root
 	p_controller.after_init()
 	m_controllers.append(p_controller)
 	return p_controller
 
+func update_tick(p_deltaTime: float):
+	for controller in m_controllers:
+		controller.update_tick(p_deltaTime)
+
+# Terminates all Views in the ViewCollection and then all tracked Controllers.
 func terminate():
 	print(str("Terminating feature:", get_class_name()))
 	
@@ -40,14 +47,14 @@ func terminate():
 	
 	m_root = null
 
+# Virtual method to implement initializing Views.
 func init_views():
 	pass
 
+# Virtual method to implement initializion Controllers.
 func init_controllers():
 	pass
 
+# Called after Views and Controllers are initialized.
 func on_initialized():
-	pass
-
-func update_tick(p_deltaTime: float):
 	pass
--- Framework/Abstract/AModelResource.gd
+++ Framework/Abstract/AModelResource.gd
@@ -1,2 +1,6 @@
-extends Resource
+# AModelResource is the base class for Models you want to save  as a Godot Resource.
+# This is useful for pre-made game data like Items/Equipment and Default Settings/Configuration.
+# Should be assigned and loaded from Features.
 class_name AModelResource
+extends Resource
+func get_class_name(): return "AModelResource"
