c578990c4d1c59e3ddd237fb24a2b1e60c208eba
turbohermit <daanlast123@gmail.com>
2024-01-07 16:14:31 +0100

    + [framework] add abstract model class

---
+++ Framework/Abstract/AModel.gd
@@ -0,0 +1,5 @@
+# AModel is the base class for Models.
+# This is used mostly for instanced data and data dynamically created at runtime.
+class_name AModel
+extends RefCounted
+func get_class_name(): return "AModelResource"
