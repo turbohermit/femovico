6fe6eb522a70bc3b7a74259d7eb30615b336fc8d
turbohermit <daanlast123@gmail.com>
2024-04-17 20:57:20 +0200

    + [framework] add optional signalling * [framework] fix controller not calling terminate signal

--- Framework/Abstract/AController.gd
+++ Framework/Abstract/AController.gd
@@ -4,15 +4,19 @@ class_name AController
 func get_class_name(): return "AController"
 
 var m_root: Node
-var m_viewCollection: ViewCollection = ViewCollection.new()
+var m_viewCollection: ViewCollection
+
+signal on_terminated(p_controller: AController)
 
 # Terminates all views in this controller's ViewCollection and unassigns local variables.
-func terminate():
+func terminate(p_signal: bool = true):
 	print(str("Terminating controller: ", get_class_name()))
+	on_terminate()
 	m_viewCollection.terminate()
 	m_viewCollection = null
 	m_root = null
-	on_terminate()
+	if p_signal:
+		on_terminated.emit(self)
 
 # Called after _init and m_root is assigned.
 func on_initialized():
