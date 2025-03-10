10e4b59e3759c10236390c83575421461963c91d
turbohermit <daanlast123@gmail.com>
2025-03-10 21:55:46 +0100

    - [example] remove example

--- Example/Scripts/ModelResources/CreatureModelResource.gd
+++
@@ -1,7 +0,0 @@
-class_name CreatureModelResource
-extends AModelResource
-
-@export_category("Creature Base Stats")
-@export var Health: int = 10
-@export var Speed: float = 0.2
-@export var Scale: float = 1.0
