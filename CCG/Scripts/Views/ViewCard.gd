816726d73b2365723c0da9e7d77a365065b89175
turbohermit <daanlast123@gmail.com>
2025-03-13 10:11:27 +0100

    + [views] add hover visuals to card view

--- CCG/Scripts/Views/ViewCard.gd
+++ CCG/Scripts/Views/ViewCard.gd
@@ -4,9 +4,26 @@ extends AView
 @export_category("Nodes")
 @export var ParentNode: Control
 @export var TitleLabel: RichTextLabel
+@export var HoverNode: Control
+
+@export_category("Visuals")
+@export var HoverSize: float = 1.2
+
+const BBCODE_CENTER: String = "[center]"
 
 func _ready():
-	pass
+	ParentNode.mouse_entered.connect(mouse_entered_received)
+	ParentNode.mouse_exited.connect(mouse_exited_received)
 
 func update(p_card: ModelResourceCard):
-	TitleLabel.text = p_card.DisplayNameKey
+	TitleLabel.text = str(BBCODE_CENTER, p_card.DisplayNameKey)
+
+func mouse_entered_received():
+	HoverNode.visible = true
+	ParentNode.scale = Vector2.ONE * HoverSize
+	ParentNode.z_index = 1
+
+func mouse_exited_received():
+	HoverNode.visible = false
+	ParentNode.scale = Vector2.ONE
+	ParentNode.z_index = 0
