1eb0ad0f0fbf8cc2905109a7e44a0fad06d3008a
turbohermit <daanlast123@gmail.com>
2025-04-28 19:34:37 +0200

    _ [example] change combat feature to adhere to conventions

--- Example/Scripts/Features/CombatFeature.gd
+++ Example/Scripts/Features/CombatFeature.gd
@@ -7,18 +7,24 @@ extends AFeature
 @export_category("View Scenes")
 @export var EnemyViewScene: PackedScene
 
-func init_controllers():
+# Models
+var m_liveEnemiesModel: LiveEnemiesModel
+var m_randomModel: RandomModel
+
+func init_models():
 	# Initializing a Model in Features is fine if you want to share it over multiple Controllers.
 	# Just make sure to actually manipulate the data from the Controllers, not in the Feature.
-	var liveEnemiesModel = LiveEnemiesModel.new()
-	var randomModel = RandomModel.new()
+	m_liveEnemiesModel = LiveEnemiesModel.new()
+	m_randomModel = RandomModel.new()
+
+func init_controllers():
 	
 	kickstart(
-		SpawnController.new(liveEnemiesModel, randomModel, SpawnerResource, EnemyViewScene)
+		SpawnController.new(m_liveEnemiesModel, m_randomModel, SpawnerResource, EnemyViewScene)
 	)
 	kickstart(
-		MovementController.new(liveEnemiesModel, randomModel, SpawnerResource)
+		MovementController.new(m_liveEnemiesModel, m_randomModel, SpawnerResource)
 	)
 	kickstart(
-		HealthController.new(liveEnemiesModel)
+		HealthController.new(m_liveEnemiesModel)
 	)
