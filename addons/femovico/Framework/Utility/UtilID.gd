98e1b191fd027493193cdb4240e4a870fce256ce
Desktop-Daan\Daan <daanlast123@gmail.com>
2026-04-14 19:40:00 +0200

    _ [id] update id utility to use stringname

--- addons/femovico/Framework/Utility/UtilID.gd
+++ addons/femovico/Framework/Utility/UtilID.gd
@@ -1,13 +1,11 @@
 @tool
 extends Node
 
+# Byte mask
 const BYTE_MASK: int = 0b11111111
 
-static func generate() -> String:
-	return generate_v4()
-
 # 16 random bytes with the bytes on index 6 and 8 modified
-static func generate_uuid_bin():
+func uuid_bin():
 	return [
 		randi() & BYTE_MASK, randi() & BYTE_MASK, randi() & BYTE_MASK, randi() & BYTE_MASK,
 		randi() & BYTE_MASK, randi() & BYTE_MASK, ((randi() & BYTE_MASK) & 0x0f) | 0x40, randi() & BYTE_MASK,
@@ -15,9 +13,9 @@ static func generate_uuid_bin():
 		randi() & BYTE_MASK, randi() & BYTE_MASK, randi() & BYTE_MASK, randi() & BYTE_MASK,
 	]
 
-static func generate_v4() -> String:
-	var b = generate_uuid_bin()
-	return '%02x%02x%02x%02x-%02x%02x-%02x%02x-%02x%02x-%02x%02x%02x%02x%02x%02x' % [
+func generate_uuid() -> StringName:
+	var b = uuid_bin()
+	return &'%02x%02x%02x%02x-%02x%02x-%02x%02x-%02x%02x-%02x%02x%02x%02x%02x%02x' % [
 		b[0], b[1], b[2], b[3],
 		b[4], b[5],
 		b[6], b[7],
